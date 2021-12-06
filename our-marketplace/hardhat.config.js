2bb4b41dfe51caaacf536c5165210544ae44cfd2
AyoubBalti <ayoub.balti@enis.tn>
2021-12-06 09:54:48 +0100

    Full Project

--- our-marketplace/hardhat.config.js
+++ our-marketplace/hardhat.config.js
@@ -1,5 +1,37 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
-    solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    
+    mumbai: {
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+        url: "https://rpc-mumbai.maticvigil.com/v1/189fd58d1006d8fc9bdf582f2123b6cce9974aec",
+      accounts: [privateKey]
+    },
+    matic: {
+      // Infura
+      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: "https://rpc-mainnet.maticvigil.com/v1/189fd58d1006d8fc9bdf582f2123b6cce9974aec",
+      accounts: [privateKey]
+    }
+    
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
+
